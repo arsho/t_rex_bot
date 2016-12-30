45bc9a8c3e87158ccdf4c3672752a49c5167bcf7
Ahmedur Rahman Shovon <shovon.sylhet@gmail.com>
2016-12-31 01:17:24 +0600

    Added initial files

---
+++ readme.md
@@ -0,0 +1,35 @@
+ <p>
+Google Chrome offline game (T Rex) bot using Python. 
+**You need to turn off your internet connection to run this program.**
+</p>
+<h2>
+<a id="how-to-use" class="anchor" href="#how-to-use" aria-hidden="true"><span class="octicon octicon-link"></span></a>How to use:
+</h2>
+
+#### Install Dependencies
+
+I prefer using Virtual environment to play with new libraries and packages so that my core packages remain unharmed.
+To use this in virtual environment open a terminal (I am using Ubuntu 15.04 and Python3) and follow the steps.
+
+1. Create and activate `virtualenv` (Virtual Environment) with `Python3`:
+
+		$ virtualenv -p python3 venv
+		$ source venv/bin/activate
+
+2. Install necesarry packages inside the `virtualenv`:
+
+		(venv)$ pip install pyautogui
+
+		
+#### Running Python code using IDLE3 in Virtual Environment
+
+To run IDLE3 while `venv` is activated use the following command. Then browse and open the file using `idle3`.
+
+		(venv)$ python -m idlelib.idle
+
+<ul>
+<li>Run t_rex_bot.py.</li>
+<li>Sit tight, bot will start playing!</li>
+</ul>
+
+## This application is still in development phase. 
---
+++ t_rex_bot.py
@@ -0,0 +1,54 @@
+import pyautogui
+import webbrowser
+import time
+def get_pixel_colour(i_x, i_y):
+    im = pyautogui.screenshot()
+    return im.getpixel((i_x, i_y))
+ 
+url = "http://google.com"
+webbrowser.open(url)
+time.sleep(1.5)
+pyautogui.press("space")
+dragon_x=505
+dragon_y=247
+bird_x=495
+bird_y=225
+black_color=(83,83,83)
+bg_color=(247,247,247)
+white_color=(255,255,255)
+#pyautogui.moveTo((dragon_x,dragon_y))
+pyautogui.moveTo((bird_x,bird_y))
+start = time.time()
+try:
+    while True:
+        c = get_pixel_colour(dragon_x, dragon_y)        
+        if c!=bg_color and c!=white_color:
+            end = time.time()
+            print(end - start)            
+            print("1. Dragon found")
+            pyautogui.press("space")
+            continue
+        c=get_pixel_colour(bird_x,bird_y)
+        if c!=bg_color and c!=white_color:            
+            end = time.time()
+            print(end - start)            
+            print("2. Bird found")
+            pyautogui.press("space")
+            continue
+        c = get_pixel_colour(dragon_x, dragon_y-2)
+        if c!=bg_color and c!=white_color:
+            end = time.time()
+            print(end - start)            
+            print("3. Dragon found")
+            pyautogui.press("space")
+            continue
+        c = get_pixel_colour(dragon_x+2, dragon_y)
+        if c!=bg_color and c!=white_color:
+            end = time.time()
+            print(end - start)            
+            print("4. Dragon found")
+            pyautogui.press("space")
+            continue         
+
+except KeyboardInterrupt:
+    print("\n")
